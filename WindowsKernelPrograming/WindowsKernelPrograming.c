4df68ee0ccf4b7d2204f737e9e2ddf76d5670485
Yyyyshen <Yyyyshen@gmail.com>
2021-01-18 17:35:09 +0800

    一些基本概念

--- WindowsKernelPrograming/WindowsKernelPrograming.c
+++ WindowsKernelPrograming/WindowsKernelPrograming.c
@@ -14,6 +14,19 @@
  * 书的开头介绍了很多驱动层开发更基本的知识
  */
 
+/**
+ * CPU运行环概念:
+ * x86计算机中，CPU有四个特权级别 ring0（特权最大）~ring3
+ * 大多数内核都只使用0和3两个级别
+ * 内核应用运行于ring0，用户应用运行于ring3
+ */
+
+/**
+ * IRQL中断级别：
+ * 高IRQL代码可以中断低IRQL代码执行过程
+ * 驱动中常见级别为0~2
+ */
+
 VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 {
 	if (DriverObject != NULL)
@@ -26,6 +39,7 @@ VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
+	//获取当前驱动所在的进程ID和IRQL级别，此处应该两个值都为0
 	DbgPrint("[%ws]Hello Kernel World, CurrentProcessId = 0x%p , CurrentIRQL = 0x%u\n", __FUNCTIONW__, PsGetCurrentProcessId(), KeGetCurrentIrql());
 	if (RegistryPath != NULL)
 	{
@@ -38,8 +52,10 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 		DriverObject->DriverUnload = DriverUnload;
 	}
 
+	//内核层编程中，大部分情况使用Unicode
 	WCHAR strBuf[128] = { 0 };
-
+	//一般不直接使用WCHAR，而是UNICODE_STRING，结构体内buffer指针所指缓冲区结尾不一定包含'\0'
+	//不依赖结束标志更为安全，可以有效防止缓冲区溢出覆盖掉\0导致的违法访问
 	UNICODE_STRING uFirstString = { 0 };
 	RtlInitEmptyUnicodeString(&uFirstString, strBuf, sizeof(strBuf));
 	RtlUnicodeStringCopyString(&uFirstString, L"Hello,Kernel\n"); //只能在PASSIVE_LEVEL下使用
