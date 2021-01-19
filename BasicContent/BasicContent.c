bcf95bcf8e9ac8cd6ab9771ba4e5dce8a43fe253
Yyyyshen <Yyyyshen@gmail.com>
2021-01-19 20:53:19 +0800

    句柄、文件、注册表操作

--- BasicContent/BasicContent.c
+++ BasicContent/BasicContent.c
@@ -1,4 +1,6 @@
+#include "ntifs.h"
 #include "ntddk.h"
+#include "ntstrsafe.h"
 
 /**
  * 驱动编程常用基础点
@@ -105,6 +107,257 @@ BOOLEAN Test_Lookaside()
 	}
 	return bSucc;
 }
+
+/**
+ * Windows把一切都作为对象来管理，进程、线程、驱动等都是对象
+ * 如果用户态进程需要创建一个内核对象，对象内存地址属于内核态地址空间，而用户态进程无法访问
+ * 就需要句柄(HANDLE),句柄就如同内核对象凭证，用户态程序可以间接操作内核对象
+ * 句柄只在当前进程中有意义，因为不同进程有各自的句柄表，内核态中，有一个系统的句柄表，存在于SYSTEM进程，只有一个，所有内核驱动都能使用该表
+ * 根据上下文概念，如果有一个函数F1在进程P1中执行，打开一个句柄H1，另一个函数F2需要在进程P2使用H1句柄，就会出现错误
+ * 可以在创建H1句柄时，指定句柄类型为内核句柄，这样就可以跨进程使用了
+ */
+BOOLEAN Test_Handle()
+{
+	BOOLEAN bSucc = FALSE;
+	HANDLE hCreateEvent = NULL;
+	PVOID pCreateEventObject = NULL;
+	HANDLE hOpenEvent = NULL;
+	PVOID pOpenEventObject = NULL;
+
+	do
+	{
+		OBJECT_ATTRIBUTES obj_attr = { 0 };
+		UNICODE_STRING uNameString = { 0 };
+		RtlInitUnicodeString(&uNameString, L"\\BaswNamedObjects\\TestEvent");
+		InitializeObjectAttributes(&obj_attr, &uNameString, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, NULL);
+		ZwCreateEvent(&hCreateEvent, EVENT_ALL_ACCESS, &obj_attr, SynchronizationEvent, FALSE);
+		if (hCreateEvent == NULL)
+		{
+			break;
+		}
+		ObReferenceObjectByHandle(hCreateEvent, EVENT_ALL_ACCESS, *ExEventObjectType, KernelMode, &pCreateEventObject, NULL);
+		if (pCreateEventObject == NULL)
+		{
+			break;
+		}
+		ZwOpenEvent(&hOpenEvent, EVENT_ALL_ACCESS, *ExEventObjectType, KernelMode, &pCreateEventObject, NULL);
+		if (hOpenEvent == NULL)
+		{
+			break;
+		}
+		ObReferenceObjectByHandle(hOpenEvent, EVENT_ALL_ACCESS, *ExEventObjectType, KernelMode, &pOpenEventObject, NULL);
+		if (pOpenEventObject == NULL)
+		{
+			break;
+		}
+		DbgPrint("Create Handle: %p, Create Pointer = %p \n", hCreateEvent, pCreateEventObject);
+		DbgPrint("Open Handle: %p, Open Pointer = %p \n", hOpenEvent, pOpenEventObject);
+		bSucc = TRUE;
+
+	} while (FALSE);
+
+	//释放
+	if (pCreateEventObject != NULL)
+	{
+		ObDereferenceObject(pCreateEventObject);
+		pCreateEventObject = NULL;
+	}
+	if (hCreateEvent != NULL)
+	{
+		ZwClose(hCreateEvent);
+		hCreateEvent = NULL;
+	}
+	if (pOpenEventObject != NULL)
+	{
+		ObDereferenceObject(pOpenEventObject);
+		pOpenEventObject = NULL;
+	}
+	if (hOpenEvent != NULL)
+	{
+		ZwClose(hOpenEvent);
+		hOpenEvent = NULL;
+	}
+
+	return bSucc;
+}
+
+/**
+ * 注册表是一种文件，Windows\System32\config下的文件被以内存映射的方式映射到内核空间，然后以一种HIVE的方式组织起来
+ * 之前项目中都用过，简单过一下
+ */
+VOID Test_Reg_Create(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+	//创建新的注册表键，只能在IRQL为PASSIVE_LEVEL下运行（Zw开头函数一般都是）
+	OBJECT_ATTRIBUTES objAttr = { 0 };
+	HANDLE hkey = NULL;
+	ULONG ulDisposition = 0;
+	UNREFERENCED_PARAMETER(DriverObject);//告诉编译器，已经使用了该变量，不必检测警告
+	InitializeObjectAttributes(&objAttr, RegistryPath, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, NULL);
+	ZwCreateKey(&hkey, KEY_WRITE, &objAttr, 0, NULL, REG_OPTION_NON_VOLATILE, &ulDisposition);
+	if (hkey != NULL)
+	{
+		//修改键
+		UNICODE_STRING usValueName = { 0 };
+		ULONG ulNewStartValue = 2;
+		RtlInitUnicodeString(&usValueName, L"Start");
+		ZwSetValueKey(hkey, &usValueName, 0, REG_DWORD, (PVOID)&ulNewStartValue, sizeof(ulNewStartValue));
+
+		//查询
+		ULONG ulRetSize = 0;
+		NTSTATUS nStatus = ZwQueryValueKey(hkey, &usValueName, KeyValuePartialInformation, NULL, 0, &ulRetSize);
+		if (nStatus == STATUS_BUFFER_TOO_SMALL && ulRetSize != 0)//空间不足
+		{
+			//ulRetSize保存的是所需大小，确定所需大小后，申请内存并再次查询
+			ULONG ulStartValue = 0;
+			PKEY_VALUE_PARTIAL_INFORMATION pData = ExAllocatePoolWithTag(PagedPool, ulRetSize, 'DriF');
+			if (pData != NULL)
+			{
+				memset(pData, 0, ulRetSize);
+				nStatus = ZwQueryValueKey(hkey, &usValueName, KeyValuePartialInformation, (PVOID)pData, ulRetSize, &ulRetSize);
+			}
+			//释放内存
+			ExFreePoolWithTag(pData, 'Drif');
+			pData = NULL;
+		}
+
+		//关闭和释放
+		ZwClose(hkey);
+		hkey = NULL;
+	}
+}
+
+/**
+ * 文件操作
+ * 路径前加“\\??\\”，因为操作时使用的时对象路径，而“C:”是一个符号链接对象，这类对象都在“\\??\\”路径下
+ * 解释了之前在应用层写程序时，从资源管理器中复制出来的路径到VS里，前面有两个问号但代码中不可见，导致一直获取不到文件
+ */
+VOID Test_File(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+	HANDLE file_handle = NULL;
+	IO_STATUS_BLOCK io_status;
+	//先初始化含有文件路径的OBJECT_ATTRIBUTES
+	OBJECT_ATTRIBUTES objAttr;
+	UNICODE_STRING ufile_name = RTL_CONSTANT_STRING(L"\\??\\C:\\Test.txt");
+	InitializeObjectAttributes(&objAttr, &ufile_name, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+	//打开文件
+	ZwCreateFile(&file_handle, GENERIC_READ | GENERIC_WRITE, &objAttr, &io_status, NULL,
+		FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE | FILE_RANDOM_ACCESS | FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+	//关闭文件
+	ZwClose(file_handle);
+}
+//拷贝操作示例
+NTSTATUS MyCopyFile(PUNICODE_STRING target_path, PUNICODE_STRING source_path)
+{
+	//文件句柄
+	HANDLE target = NULL;
+	HANDLE source = NULL;
+
+	//拷贝缓冲区
+	PVOID buffer = NULL;
+	LARGE_INTEGER offset = { 0 };
+	IO_STATUS_BLOCK io_status = { 0 };
+
+	NTSTATUS status;
+	do
+	{
+		//打开文件句柄
+		OBJECT_ATTRIBUTES objAttr_target;
+		OBJECT_ATTRIBUTES objAttr_source;
+		UNICODE_STRING u_target = *target_path;
+		UNICODE_STRING u_source = *source_path;
+		InitializeObjectAttributes(&objAttr_target, &u_target, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+		InitializeObjectAttributes(&objAttr_source, &u_source, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+		ZwCreateFile(&target, GENERIC_READ | GENERIC_WRITE, &objAttr_target, &io_status, NULL,
+			FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE | FILE_RANDOM_ACCESS | FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+		ZwCreateFile(&source, GENERIC_READ | GENERIC_WRITE, &objAttr_source, &io_status, NULL,
+			FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OPEN_IF, FILE_NON_DIRECTORY_FILE | FILE_RANDOM_ACCESS | FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+		if (target == NULL || source == NULL)
+		{
+			break;
+		}
+
+		//给buffer分配内存
+		int length = 4 * 1024;
+		buffer = ExAllocatePool(PagedPool, length);
+		if (buffer == NULL)
+		{
+			break;
+		}
+		memset(buffer, 0, length);
+
+		//循环读写
+		while (1)
+		{
+			length = 4 * 1024;//每次想要读的长度
+			status = ZwReadFile(source, NULL, NULL, NULL, &io_status, buffer, length, &offset, NULL);
+			if (!NT_SUCCESS(status))
+			{
+				//判断是否读完
+				if (status == STATUS_END_OF_FILE)
+				{
+					status = STATUS_SUCCESS;
+					break;
+				}
+			}
+			length = io_status.Information;//实际读到的长度
+			//写入文件
+			status = ZwWriteFile(target, NULL, NULL, NULL, &io_status, buffer, length, &offset, NULL);
+			if (!NT_SUCCESS(status))
+			{
+				break;
+			}
+			//移动偏移量，继续循环，直到读完
+			offset.QuadPart += length;
+		}
+
+	} while (0);
+
+	//回收资源
+	if (target != NULL)
+	{
+		ZwClose(target);
+	}
+	if (source != NULL)
+	{
+		ZwClose(source);
+	}
+	if (buffer != NULL)
+	{
+		ExFreePool(buffer);
+	}
+
+	return STATUS_SUCCESS;
+
+}
+
+/**
+ * 驱动中等待或停顿会使整个系统卡住，应启动另一个线程来做一些长期、耗时的操作
+ * 驱动中生成的线程一般是系统线程，所在进程为System
+ */
+VOID MyThreadProc(PVOID context)
+{
+	PUNICODE_STRING str = (PUNICODE_STRING)context;
+	DbgPrint("Print something in my thread.");
+	PsTerminateSystemThread(STATUS_SUCCESS);//结束自己
+}
+VOID Test_Thread()
+{
+	UNICODE_STRING str = { 0 };
+	RtlInitUnicodeString(&str, L"Hello!");
+	HANDLE thread = NULL;
+	NTSTATUS status = STATUS_SUCCESS;
+	status = PsCreateSystemThread(&thread, 0, NULL, NULL, NULL, MyThreadProc, (PVOID)&str);
+	//上面写法这里有个问题，当MyThreadProc执行时，本方法可能执行完毕了，堆栈中str可能已经释放，会导致蓝屏，所以要将str放在全局空间
+	//或者在后面加上等待线程结束的语句
+	if (!NT_SUCCESS(status))
+	{
+		//错误处理
+	}
+	//关闭句柄
+	ZwClose(thread);
+}
+//同步事件
+
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
