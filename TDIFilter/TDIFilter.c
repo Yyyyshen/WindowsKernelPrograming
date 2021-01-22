eb43bfd5954e3cdda0f04d57752eed7a55e008a1
Yyyyshen <Yyyyshen@gmail.com>
2021-01-22 16:20:19 +0800

    TDI过滤驱动基本逻辑理清

--- TDIFilter/TDIFilter.c
+++ TDIFilter/TDIFilter.c
@@ -17,6 +17,8 @@
   *
   * PS: TDIFW——一个开源的TDI防火墙
   * https://sourceforge.net/projects/tdifw/files/latest/download
+  * 可将这个例子编译为静态库，留出一些必要回调接口，之后就可以在这个库基础上进行TDI防火墙开发了
+  * 具体使用可以去找下《Windows内核编程》随书源码tdifw_smpl
   */
 
   /**
@@ -102,6 +104,8 @@ enum {
 enum {
 	IRP_CREATE = 1,
 	IRP_CONTROL,
+	IRP_INTERNAL_CONTROL,
+	IRP_CLEANUP,
 	IRP_CLOSE
 };
 
@@ -309,7 +313,25 @@ int deal_tdi_create(PIRP irp, PIO_STACK_LOCATION irps, struct _completion* compl
 
 	return FILTER_ALLOW;
 }
-//完成请求
+//在连接进入事件中设置自定义回调回调函数
+typedef struct {
+	PFILE_OBJECT fileobj;
+	PVOID old_handler;
+	PVOID old_context; //就是回调函数的第一个参数
+}TDI_EVENT_CONTEXT; //自定义上下文信息结构
+NTSTATUS my_handler(PVOID TdiEventContext, LONG RemoteAddressLength, PVOID RemoteAddress, LONG UserDataLength, PVOID UserData,
+	LONG OptionsLength, PVOID Options, CONNECTION_CONTEXT* ConnectionContext, PIRP* AcceptIrp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+	//执行自己逻辑
+
+	//调用原来的回调函数
+	TDI_EVENT_CONTEXT* ctx = (TDI_EVENT_CONTEXT*)TdiEventContext;
+	status = ((PTDI_IND_CONNECT)(ctx->old_handler))(ctx->old_context, //转为原函数指针后调用
+		RemoteAddressLength, RemoteAddress, UserDataLength, UserData, OptionsLength, Options, ConnectionContext, AcceptIrp);
+	return status;//若返回STATUS_CONNECTION_REFUSED，请求将不能建立
+}
+//进一步处理逻辑/次功能号，之后完成本次请求
 NTSTATUS
 tdi_dispatch_complete(PDEVICE_OBJECT devobj, PIRP irp, int filter, PIO_COMPLETION_ROUTINE cr, PVOID context, int irp_type)
 {
@@ -344,6 +366,91 @@ tdi_dispatch_complete(PDEVICE_OBJECT devobj, PIRP irp, int filter, PIO_COMPLETIO
 		}
 		else if (irp_type == IRP_CONTROL)
 		{
+			//内核中netbt设备发送TCP数据时，无法被TDI过滤驱动捕获TDI_SEND请求
+			//netbt时基于TCP/IP的NetBIOS协议，用于计算机名字解析
+			//netbt能直接获取TCP协议驱动中内部函数TCPSendData指针，用这个函数直接发送数据，就绕过了TDI_SEND
+			//通过对tcpip.sys的研究，有一个功能号为IOCTL_TDI_QUERY_DIRECT_SEND_HANDLER
+			BOOLEAN bRet = irps->Parameters.DeviceIoControl.IoControlCode == IOCTL_TDI_QUERY_DIRECT_SEND_HANDLER;
+			//如果发送这个请求，tcpip.sys将把TCPSendData函数指针返回，可以用其发送数据
+			//所以只要对此特殊处理，将返回的TCPSendData函数保存，取代为自己的过滤函数即可
+			if (bRet)
+			{
+				VOID* buff = irps->Parameters.DeviceIoControl.Type3InputBuffer;
+				if (buff != NULL)
+				{
+					//old_TCPSendData = *(TCPSendData_t**)buff;
+					//*(TCPSendData_t**)buff = my_TCPSendData;
+				}
+			}
+		}
+		else if (irp_type == IRP_INTERNAL_CONTROL)
+		{
+			//处理各次功能号的请求
+
+			if (irps->MinorFunction == TDI_ASSOCIATE_ADDRESS)
+			{
+				//TDI_ASSOCIATE_ADDRESS，用于把传输层对象和连接对象联系起来
+				//在这样的请求中，IRP当前栈空间的FileObjcet域中保存的文件对象指针时连接终端的文件对象
+				//我们之前保存过连接上下文指针，以文件对象指针为索引可以取到
+				//把两组信息联系起来之后，当我们得到一个连接上下文对象时，就能知道所使用的本地地址
+				//与之对应的，TDI_DISASSOCIATE_ADDRESS，删除地址的文件对象与连接上下文之间的关系
+			}
+
+			if (irps->MinorFunction == TDI_CONNECT)
+			{
+				//TDI_CONNECT
+				//这个请求发生在本地试图连接外界时，当这个请求完成时，连接就已经建立了，这里也是监控网络安全很重要的地方
+				//可在此处控制：进程使用本地什么地址、进程试图连接什么远程地址、是否允许访问发生、是否记录日志等
+
+				//获取远程地址
+				PTDI_REQUEST_KERNEL_CONNECT param = (PTDI_REQUEST_KERNEL_CONNECT)(&irps->Parameters);
+				TA_ADDRESS* remote_addr = ((TRANSPORT_ADDRESS*)(param->RequestConnectionInformation->RemoteAddress))->Address;
+
+				//根据得到的信息做相应处理
+			}
+
+			//还有一些功能号：TDI_SEND、TDI_RECEIVE、TDI_SEND_DATAGRAM、TDI_RECEIVE_DATAGRAM 处理传输过程
+			//两种传输方式：流式传输（对应前两个功能号）和报式传输
+			//流式传输不关心每次传输多少，只关心连接上发送的总大小，先发送的永远先到（TCP
+			//报式传输没有连接，一次发送一个数据包，后发送的包不一定在先发送的包之后被接收（UDP
+			//可以在传输过程做过滤处理：检查数据、检测病毒、加密数据、修改数据、拒绝发送、备份数据
+			if (irps->MinorFunction == TDI_SEND)
+			{
+				//获取文件对象
+				PFILE_OBJECT fileObj = irps->FileObject;
+				//find方法获取之前保存的连接对象
+
+				//获取数据，读取MDL中数据即可
+				VOID* buff = MmGetSystemAddressForMdl(irp->MdlAddress);//获取实际缓冲区位置
+				ULONG len = irp->IoStatus.Information;//获取长度
+
+				//禁止发送或接收，把这个请求返回错误即可
+			}
+
+			//TDI_SET_EVENT_HANDLER
+			//socket调用listen时，一个类型为TDI_EVENT_CONNECT设置事件请求将发送到下层协议，下层协议得到一个回调函数指针
+			//TDI_SET_EVENT_HANDLER是一个设置事件回调的请求
+			if (irps->MinorFunction == TDI_SET_EVENT_HANDLER)
+			{
+				//事件种类很多，先要获取类型
+				PTDI_REQUEST_KERNEL_SET_EVENT r = (PTDI_REQUEST_KERNEL_SET_EVENT)&irps->Parameters;
+				LONG type = r->EventType;//事件类型
+				r->EventHandler;//回调函数，根据事件类型不同，回调函数也不同
+				//前面提过TDI_CONNECT过滤只能针对本地连外部
+				//对于外部连接本地，则通过TDI_EVENT_CONNECT类型事件，设置回调函数，我们可以用自己的回调函数取代它，并保存原回调函数指针
+				//执行完自己的回调，再调用原本的函数，实现侦听
+				if (type == TDI_EVENT_CONNECT && r->EventHandler != NULL)
+				{
+					TDI_EVENT_CONTEXT* ctx;//自定义上下文结构存储原来回调函数信息
+					ctx->fileobj = irps->FileObject;
+					ctx->old_handler = r->EventHandler;
+					ctx->old_context = r->EventContext;
+					//替换为自己的
+					r->EventHandler = my_handler;
+					r->EventContext = ctx;
+				}
+			}
+
 		}
 		else if (irp_type == IRP_CLOSE)
 		{
@@ -384,15 +491,26 @@ NTSTATUS DeviceDispatch(PDEVICE_OBJECT DeviceObject, PIRP irp)
 			break;
 		}
 		case IRP_MJ_DEVICE_CONTROL: //设备控制
-			status = tdi_dispatch_complete(DeviceObject, irp, result, completion.routine, completion.context, IRP_CONTROL);
-			break;
+			//这种请求与IRP_MJ_INTERNAL_DEVICE_CONTROL基本重复（除了netbt发送数据问题）
+			status = TdiMapUserRequest(DeviceObject, irp, irps);//可以调用该函数将IRP_MJ_DEVICE_CONTROL转换为IRP_MJ_INTERNAL_DEVICE_CONTROL
+
+			status = tdi_dispatch_complete(DeviceObject, irp, FILTER_ALLOW, completion.routine, completion.context, IRP_CONTROL);
+
+			//break; //这里可以不break，直接作为内部控制请求继续处理
 		case IRP_MJ_INTERNAL_DEVICE_CONTROL: //内部设备控制
+			//上下两种功能号在TDI中功能基本相同
+			status = tdi_dispatch_complete(DeviceObject, irp, FILTER_ALLOW, completion.routine, completion.context, IRP_INTERNAL_CONTROL);
+			break;
+		case IRP_MJ_CLEANUP://清理
+			//此处一般用于删除保存的连接和地址信息
+			//收到清理请求意为着一个文件对象的句柄降到0，但并不意味着文件对象的引用计数一定降为0，因为引用计数不止是打开句柄时增加
+			//当引用计数减少为0时，文件对象会销毁，此时才会收到CLOSE请求
+			status = tdi_dispatch_complete(DeviceObject, irp, FILTER_ALLOW, completion.routine, completion.context, IRP_CLEANUP);
 			break;
 		case IRP_MJ_CLOSE: //关闭
-			status = tdi_dispatch_complete(DeviceObject, irp, result, completion.routine, completion.context, IRP_CLOSE);
+			status = tdi_dispatch_complete(DeviceObject, irp, FILTER_ALLOW, completion.routine, completion.context, IRP_CLOSE);
 			break;
-		case IRP_MJ_CLEANUP:
-			break;
+
 		default:
 			break;
 		}
