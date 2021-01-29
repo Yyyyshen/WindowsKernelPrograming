d3304da62bb59a985dbe772beaa0e7949e6ba45d
Yyyyshen <Yyyyshen@gmail.com>
2021-01-29 15:13:20 +0800

    需要一个与WFP驱动交互的用户程序

---
+++ WFPExec/WFPExecDlg.h
@@ -0,0 +1,33 @@
+﻿
+// WFPExecDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CWFPExecDlg 对话框
+class CWFPExecDlg : public CDialogEx
+{
+// 构造
+public:
+	CWFPExecDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WFPEXEC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
